f98f156c51fc7349b785d09fc79e980ac691acb2
Nick-Maldonado <nickmaldonado94@gmail.com>
2018-06-19 18:29:15 -0500

    created simple server

---
+++ server.js
@@ -0,0 +1,21 @@
+const http = require('http');
+const port = 3000;
+
+const requestHandler = (request, response) => {
+  response.end(
+    fs.writeFile('hello-world.txt', 'HelloWorld', 'utf8', (err) => {
+      if (err) throw err;
+      console.log('Created hello-world.txt')
+    })
+  )
+};
+
+const server = http.createServer(requestHandler);
+
+server.listen(port, (err) => {
+  if (err) {
+    return console.log(`You have an error: ${err}`);
+  }
+
+  console.log(`sever is listening on ${port}`);
+});
